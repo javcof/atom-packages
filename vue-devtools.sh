ddd0d112311be9b5e8fdd58fb1a5f9e6e1e8aca3
javcof <javcof@qq.com>
2018-02-06 17:59:15 +0800

    Vue devtools setup shell

---
+++ vue-devtools.sh
@@ -0,0 +1,14 @@
+# config
+npm config set loglevel=http
+
+# Chrome extensions
+## Clone this repo
+## npm install (or yarn install) if you are using yarn as the package manager
+## npm run build
+## Open chrome extension page
+## Check "developer mode"
+## Click "load unpacked extension", and choose shells/chrome
+cd ~/Documents/Github/
+git clone https://github.com/vuejs/vue-devtools.git
+cd vue-devtools/
+npm run build
