e923b4c37d1904e91861c1e50e64c9edf82517af
javcof <javcof@qq.com>
2018-02-09 15:12:12 +0800

    Add atom-vue package

---
+++ scripts/atom-vue.sh
@@ -0,0 +1,11 @@
+# config
+npm config set loglevel=http
+
+# hedefalk/atom-vue
+# Vue component file syntax for atom
+apm uninstall atom-vue
+cd ~/.atom/packages/
+git clone https://github.com/hedefalk/atom-vue.git
+cd atom-vue/
+npm install
+cd ~
